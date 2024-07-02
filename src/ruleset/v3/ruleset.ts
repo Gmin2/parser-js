849f3bce34c04991ac6b2f6680b9b89bd1a409f8
utnim2 <mintugogoi567@gmail.com>
2024-07-02 23:11:49 +0530

    added v3 rules for `channel-server`

--- src/ruleset/v3/ruleset.ts
+++ src/ruleset/v3/ruleset.ts
@@ -2,6 +2,7 @@
 
 import { AsyncAPIFormats } from '../formats';
 import { operationMessagesUnambiguity } from './functions/operationMessagesUnambiguity';
+import { channelServers3 } from './functions/channelServers3';
 import { pattern } from '@stoplight/spectral-functions';
 
 export const v3CoreRuleset = {
@@ -56,6 +57,16 @@ export const v3CoreRuleset = {
           match: '#\\/servers\\/', // If doesn't match, rule fails.
         },
       },
-    }
+    },
+    'asyncapi3-channel-servers': {
+      description: 'Channel servers must be defined in the root "servers" object.',
+      message: '{{error}}',
+      severity: 'error',
+      recommended: true,
+      given: '$',
+      then: {
+        function: channelServers3,
+      },
+    },
   },
 };
